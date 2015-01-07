f168c7f90e33ce41a06de797e0c4219749a68ac7
Bradley Ryan <bradryan13@gmail.com>
2015-01-06 20:01:11 -0700

    Add parallax and icons

--- wp-content/themes/rookierugby/views/page-home.php
+++ wp-content/themes/rookierugby/views/page-home.php
@@ -8,10 +8,10 @@ get_header();
 ?>
 
 <div id="hero">
-
+    <div class="hero-bg" data-0="-webkit-transform: translateY(0px) translateZ(0px);" data-1000="-webkit-transform: translateY(250px) translateZ(0px);"></div>
     <div class="wrapper">
 
-        <div class="caption" data-0="opacity: 1" data-400="opacity: 0" >
+        <div class="caption" data-0="opacity: 1; -webkit-transform: translateY(0px) translateZ(0px);" data-800="opacity: 0; -webkit-transform: translateY(250px) translateZ(0px);" >
             <h2>Safe, Non-Contact Rookie Rugby.</h2>
             <p>Rookie Rugby is an easy to play game for all ages! The rules are simple, and minimal equipment is required.</p>
         </div>
