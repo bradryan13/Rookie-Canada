57d3ba8c3cb6043233cdcc8a3a2cf287408a1c2a
Bradley Ryan <bradryan13@gmail.com>
2015-01-07 10:20:53 -0700

    Ipad Responsive

--- wp-content/themes/rookierugby/views/partials/overview.php
+++ wp-content/themes/rookierugby/views/partials/overview.php
@@ -42,14 +42,14 @@
 
 <div class="row">
 
-  <div class="medium-16 columns">
+  <div class="large-16 columns">
     <div class="inner">
             <div id="featured-tag">Featured</div>
             <?php echo do_shortcode( '[new_royalslider id="1"]' ); ?>
     </div>
 </div>
 
-<div class="medium-8 columns" id="ad">
+<div class="large-8 columns" id="ad">
     <div class="inner">
         <div><a href="#"><img src="<?php echo get_template_directory_uri();?>/img/under-armour.png"></a></div>
     </div>
