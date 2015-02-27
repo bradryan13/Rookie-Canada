ccbb5fc8e86bfda24df0f06dff80800c116b718f
Bradley Ryan <bradryan13@gmail.com>
2015-02-26 17:25:03 -0700

    home switcher

--- wp-content/themes/rookierugby/views/page-home.php
+++ wp-content/themes/rookierugby/views/page-home.php
@@ -30,7 +30,9 @@ get_header();
     <i class="icon-close"></i>
     <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/img/large-logo.png"></a>
     <h4>Please select your primary language:</h4>
-    <div class="ui english button ghost"><a href="<?php echo home_url(); ?>/en">English</a></div><div class="ui button french ghost"><a href="<?php echo home_url(); ?>/fr">Français</a></div>
+
+<ul><?php pll_the_languages();?></ul>
+
 </div>
 
 
